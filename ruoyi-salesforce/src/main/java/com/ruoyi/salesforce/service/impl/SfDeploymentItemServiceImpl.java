b97879fb774f300bcfc527296314f0ff10eb20d6
61979 <yijian.lin@runner-corp.com.cn>
2026-01-21 23:11:14 +0800

    系统优化，增加级联删除、前端优化、增加复制功能

---
+++ ruoyi-salesforce/src/main/java/com/ruoyi/salesforce/service/impl/SfDeploymentItemServiceImpl.java
@@ -0,0 +1,11 @@
+package com.ruoyi.salesforce.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.ruoyi.salesforce.domain.SfDeploymentItem;
+import com.ruoyi.salesforce.mapper.SfDeploymentItemMapper;
+import com.ruoyi.salesforce.service.ISfDeploymentItemService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SfDeploymentItemServiceImpl extends ServiceImpl<SfDeploymentItemMapper, SfDeploymentItem> implements ISfDeploymentItemService {
+}
